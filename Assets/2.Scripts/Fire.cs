f6bb09371e3a355cdccb705ae9124f837730d9c5
morecleverer <kimdk0824@naver.com>
2021-08-28 17:12:38 +0900

    02.에셋 추가/ 맵 배치
    에셋 += 좀비무버, 스카이박스

--- Assets/2.Scripts/Fire.cs
+++ Assets/2.Scripts/Fire.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class Fire : MonoBehaviour
 {
+    public int speed = 1000;
     public GameObject bullet;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
@@ -18,7 +21,7 @@ public class Fire : MonoBehaviour
         {
             GameObject projectile = Instantiate(bullet, transform.position, Quaternion.identity) as GameObject; //Spawns the selected projectile
 
-            projectile.GetComponent<Rigidbody>().AddForce(transform.forward*5000);
+            projectile.GetComponent<Rigidbody>().AddForce(transform.forward*speed);
             
         }
 
